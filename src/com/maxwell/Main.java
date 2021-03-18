7bef3b52043ca1dfc8abf2fa7b8a4755b404f8bd
Maxwell <maxwell.guru.1@gmail.com>
2021-03-18 12:43:18 -0600

    Cleaned up a bit

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -8,33 +8,22 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-    /* Program will divide the smallest random number into the largest random number for an answer.
-        Divide by zero is of course not allowed and checked for if necessary.
-        Program will show the user the question with small and large random numbers previously generated.
-        The user will input their answer and hit return.
-        Program will inform the user if their answer was correct or not and display the full problem and answer if they are incorrect.
-        If user is correct the output will show “You are correct!”
-        The program will then say “Good Bye!” and terminate.*/
 
-        //Display Welcome Msg
+        //Display Welcome Msg purpose and reminder txt
         System.out.println("Welcome to the Division Quiz Program");
-        //Display Program Purpose
         System.out.println("This program will generate a division quiz with random numbers");
-        //Display reminder txt
         System.out.println("Please note: You may need to round your answer if using integers and only two decimal places");
 
         //Randomize two numbers w 2 Decimal and NO ZERo
 
         double number1 = (double) (Math.random() * 100);
             if (number1 == 0) {
-                System.out.println("The universe imploded... generating non-zero number");}
-                else {
-            }
+                System.out.println("The universe imploded... generating non-zero number"); }
+                else {}
         Double number2 = (double) (Math.random() * 25);
         if (number2 == 0) {
             System.out.println("The universe imploded... generating non-zero number");}
-        else {
-        }
+        else {}
 
         //Format those bad boys
         NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
@@ -47,11 +36,6 @@ public class Main {
         System.out.println(decimalFormat.format(number2));
 
 
-        //Ensure No zero is given
-
 
         }
-                //Divide the smaller into the larger
-
-
     }
