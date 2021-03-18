6997fbd51e948174d3fb3d1c77189660ee1835d5
Maxwell <maxwell.guru.1@gmail.com>
2021-03-18 10:18:19 -0600

    Initial commit

---
+++ src/com/maxwell/Main.java
@@ -0,0 +1,20 @@
+package com.maxwell;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /*  The program starts by displaying “Welcome to the Division Quiz Program”.
+        Your code then outputs the purpose of the program (To generate a random division quiz question)
+        Remind users they may need to round their answer if using integers and only two decimal places.
+        The Program will randomize two numbers.
+        Generated large random number is 0-100 and smaller random number is 0-25.
+        Program output must not exceed two decimal places.
+        Program will divide the smallest random number into the largest random number for an answer.
+        Divide by zero is of course not allowed and checked for if necessary.
+        Program will show the user the question with small and large random numbers previously generated.
+        The user will input their answer and hit return.
+        Program will inform the user if their answer was correct or not and display the full problem and answer if they are incorrect.
+        If user is correct the output will show “You are correct!”
+        The program will then say “Good Bye!” and terminate.*/
+    }
+}
